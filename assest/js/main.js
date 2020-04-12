df508633b9d87af163537b59a9b673adf7734b35
Mo29Emad <mido_barody95@yahoo.com>
2020-04-12 21:14:51 +0200

    final-touch

---
+++ assest/js/main.js
@@ -0,0 +1,63 @@
+$(window).ready(function () {
+    
+    "use strict";
+    
+    // Toggle Navbar
+    
+    $(".our-navbar .left").click(function () {
+        
+        $(".our-navbar .nav-list").fadeToggle();
+    })
+    
+    // our services slick
+    
+    $(".our-services .slider-items").slick({
+        
+        rtl: true,
+        slidesToShow: 3,
+        nextArrow: ".left",
+        prevArrow: ".right",
+        responsive: [
+            {
+                breakpoint: 1024,
+                settings: {
+                    slidesToShow: 2,
+                }
+            },
+            {
+                breakpoint: 600,
+                settings: {
+                    slidesToShow: 1,
+                }
+            }
+        ]
+        
+    });
+    
+    // our client slick
+    
+    $(".our-client .slider-items").slick({
+        
+        rtl: true,
+        slidesToShow: 4,
+        nextArrow: ".left",
+        prevArrow: ".right",
+        responsive: [
+            {
+                breakpoint: 1024,
+                settings: {
+                    slidesToShow: 3,
+                }
+            },
+            {
+                breakpoint: 767,
+                settings: {
+                    slidesToShow: 2,
+                }
+            }
+        ]
+        
+    });
+    
+    
+})
